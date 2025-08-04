9f8d3822406476f76942e6675e8d1f1e96fc2175
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-04 11:51:40 +0300

    String reverse implementation

--- src/main/java/org/strings/StringUtils.java
+++ src/main/java/org/strings/StringUtils.java
@@ -3,7 +3,14 @@ package org.strings;
 public interface StringUtils {
 
     //  reverse the order of the characters in an efficient manner.
-    default String reverseText(String str) {
-        return "";
+    static String reverseText(String str) {
+        char[] chars = str.toCharArray();
+        final int arrayLength = chars.length;
+        for (int i = 0; i < arrayLength / 2; i++) {
+            var tmp = chars[i];
+            chars[i] = chars[arrayLength - i - 1];
+            chars[arrayLength - i - 1] = tmp;
+        }
+        return String.valueOf(chars);
     }
 }
